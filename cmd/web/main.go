3957e81e16b9a1f8a39b4b2494eb846a3503922d
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-30 11:45:45 -0400

    update main.go to handle command-line flags

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,12 +3,26 @@ package main
 
 // import logging and net/http
 import (
+	"flag" // handle command-line glags and arguments
 	"log"
 	"net/http"
 )
 
 // define main point of entry
 func main() {
+
+	// define a new command-line flag with name 'addr', a adefault value of ":4000"
+	// and short help text explaining what the flag controls. The value of the flag
+	// will be stored in the addr variable at runtime
+	addr := flag.String("addr", ":4000", "HTTP network addres")
+
+	// use flag.Parse() function to parse the command-line flag
+	// This reads in the command-line glaf value and assigns it to the
+	// addr variable. You need to call this _before_ you use the addr variable,
+	// otherwise it will always contain the default value of ":4000"
+	// application will be terminated in case of any errors
+	flag.Parse()
+
 	// initialize servemux
 	mux := http.NewServeMux()
 
@@ -28,9 +42,12 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)     // fixed path
 	mux.HandleFunc("/snippet/create", snippetCreate) // fixed path
 
-	// log to console
-	log.Print("Starting server on port :4000")
-	err := http.ListenAndServe(":4000", mux)
+	// The value returned form the flag.String() function is a pointer to the flag
+	// value, not the value itself. The dereference of the pointer is needed before the usage.
+	//(prefix it with *)
+	// use log.Printf() to interpolate the address with the log message
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	// in case of errors log and exit
 	log.Fatal(err)
 }
